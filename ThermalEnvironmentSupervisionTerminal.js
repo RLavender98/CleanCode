b5d87e035ae3b0d50cbb5577da8ca67ae6b9048c
Ruby Lavender <Ruby.Lavender@softwire.com>
2022-02-11 10:42:49 +0000

    Day 5 part two refactored

--- ThermalEnvironmentSupervisionTerminal.js
+++ ThermalEnvironmentSupervisionTerminal.js
@@ -1,134 +1,159 @@
 class IntCodeComputer {
-    constructor(instructionReader, instructionExecutor) {
-        this.instructionReader = instructionReader;
+    constructor(instructionExecutor) {
         this.instructionExecutor = instructionExecutor;
     }
 }
 
 class InstructionReader {
-    readInstruction = (instruction) => {
-        const code = instruction % 100;
-        return [code, this.getParameterModes(instruction, code)];
+    readOpcode = (instruction) => {
+        return instruction % 100;
     }
 
-    getParameterModes = (instruction, code) => {
+    readParameterModes = (instruction, instructionRunner) => {
         const parameterModes = [];
-        let parameterInstruction = (instruction - code) / 100;
-        if(code === 1 || code === 2 || code === 7 || code === 8) {
-            for (let i = 0; i < 3; i++) {
+        let parameterInstruction = (instruction - instructionRunner.opcode) / 100;
+            for (let i = 0; i < instructionRunner.numberOfParameters; i++) {
                 let currentMode = parameterInstruction % 10;
                 parameterModes.push(currentMode);
                 parameterInstruction = (parameterInstruction - currentMode) / 10;
             }
             return parameterModes;
-        } else if (code === 5 || code === 6){
-            for (let i = 0; i < 2; i++) {
-                let currentMode = parameterInstruction % 10;
-                parameterModes.push(currentMode);
-                parameterInstruction = (parameterInstruction - currentMode) / 10;
-            }
-            return parameterModes;
-        } else if (code === 4) {
-            parameterModes.push(parameterInstruction % 10);
-            return parameterModes;
-        } else if (code === 3) {
-            return parameterModes;
-        } else {
-            console.log('Error reading instruction');
-        }
     }
 }
 
 
 class InstructionRunner {
+    constructor(opcode, numberOfParameters) {
+        this.opcode = opcode;
+        this.numberOfParameters = numberOfParameters;
+    }
+
     getValue = (input, position, parameterMode) => {
         if (parameterMode === 0) {
             return input[input[position]];
         }
         return input[position];
     }
+
+    skipToNextInstruction(currentPosition) {
+        return currentPosition + this.numberOfParameters + 1;
+    }
 }
 
 class InstructionExecutor {
     constructor(instructionRunners) {
         this.instructionRunners = instructionRunners;
+        this.instructionReader = new InstructionReader();
     }
 
-    executeInstruction = (instruction, input, currentPosition, operationInput) => {
+    executeInstruction = (input, currentPosition, operationInput) => {
+        const opcode = this.instructionReader.readOpcode(input[currentPosition]);
+        const instruction = [opcode, this.instructionReader.readParameterModes(input[currentPosition], this.instructionRunners[opcode - 1])]
         return this.instructionRunners[instruction[0] - 1].runInstruction(input, currentPosition, instruction[1], operationInput);
     }
 }
 
 class InstructionOneRunner extends InstructionRunner {
+    constructor() {
+        super(1,3);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         input[input[currentPosition + 3]] = this.getValue(input, currentPosition + 1, parameterModes[0])
             + this.getValue(input, currentPosition + 2, parameterModes[1]) ;
-        return currentPosition + 4;
+        return this.skipToNextInstruction(currentPosition);
     }
 }
 
 class InstructionTwoRunner extends InstructionRunner {
+    constructor() {
+        super(2,3);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         input[input[currentPosition + 3]] = this.getValue(input, currentPosition + 1, parameterModes[0])
             * this.getValue(input, currentPosition + 2, parameterModes[1]) ;
-        return currentPosition + 4;
+        return this.skipToNextInstruction(currentPosition);
     }
 }
 
 class InstructionThreeRunner extends InstructionRunner {
+    constructor() {
+        super(3,1);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         input[input[currentPosition + 1]] = operationInput;
-        return currentPosition + 2;
+        return this.skipToNextInstruction(currentPosition);
     }
 }
 
 class InstructionFourRunner extends InstructionRunner {
+    constructor() {
+        super(4,1);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         console.log(this.getValue(input, currentPosition + 1, parameterModes[0]));
-        return currentPosition + 2;
+        return this.skipToNextInstruction(currentPosition);
     }
 }
 
 class InstructionFiveRunner extends InstructionRunner {
+    constructor() {
+        super(5,2);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         if(this.getValue(input, currentPosition + 1, parameterModes[0]) !== 0) {
             return this.getValue(input, currentPosition + 2, parameterModes[1]);
         } else {
-            return currentPosition + 3;
+            return this.skipToNextInstruction(currentPosition);
         }
     }
 }
 
 class InstructionSixRunner extends InstructionRunner {
+    constructor() {
+        super(6,2);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         if(this.getValue(input, currentPosition + 1, parameterModes[0]) === 0) {
             return this.getValue(input, currentPosition + 2, parameterModes[1]);
         } else {
-            // QQ set a property on each instruction runner which lets us get the number of parameters
-            return currentPosition + 3;
+            return this.skipToNextInstruction(currentPosition);
         }
     }
 }
 
 class InstructionSevenRunner extends InstructionRunner {
+    constructor() {
+        super(7,3);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         if(this.getValue(input, currentPosition + 1, parameterModes[0]) < this.getValue(input, currentPosition + 2, parameterModes[1])) {
             input[input[currentPosition + 3]] = 1;
         } else {
             input[input[currentPosition + 3]] = 0;
         }
-        return currentPosition + 4;
+        return this.skipToNextInstruction(currentPosition);
     }
 }
 
 class InstructionEightRunner extends InstructionRunner {
+    constructor() {
+        super(8,3);
+    }
+
     runInstruction = (input, currentPosition, parameterModes, operationInput) => {
         if(this.getValue(input, currentPosition + 1, parameterModes[0]) === this.getValue(input, currentPosition + 2, parameterModes[1])) {
             input[input[currentPosition + 3]] = 1;
         } else {
             input[input[currentPosition + 3]] = 0;
         }
-        return currentPosition + 4;
+        return this.skipToNextInstruction(currentPosition);
     }
 }
 
@@ -143,11 +168,10 @@ const ThermalEnvironmentSupervisionTerminal = (input, operationInput) => {
         new InstructionSevenRunner(),
         new InstructionEightRunner()
     ];
-    const intCodeComputer = new IntCodeComputer(new InstructionReader, new InstructionExecutor(instructionRunners));
+    const intCodeComputer = new IntCodeComputer(new InstructionExecutor(instructionRunners));
     let currentPosition = 0;
     while(input[currentPosition] !== 99) {
-        const instruction = intCodeComputer.instructionReader.readInstruction(input[currentPosition]);
-        currentPosition = intCodeComputer.instructionExecutor.executeInstruction(instruction, input, currentPosition, operationInput);
+        currentPosition = intCodeComputer.instructionExecutor.executeInstruction(input, currentPosition, operationInput);
     }
     return input[0];
 }
@@ -166,3 +190,5 @@ ThermalEnvironmentSupervisionTerminal([3,225,1,225,6,6,1100,1,238,225,104,0,1102
     2,223,1005,224,554,101,1,223,223,1008,677,677,224,1002,223,2,223,1006,224,569,1001,223,1,223,1107,677,677,224,102,2,223,223,1005,224,584,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,599,1001,
     223,1,223,8,677,226,224,1002,223,2,223,1005,224,614,1001,223,1,223,7,226,677,224,1002,223,2,223,1006,224,629,101,1,223,223,1107,677,226,224,1002,223,2,223,1005,224,644,1001,223,1,223,1107,226,677,
     224,102,2,223,223,1006,224,659,1001,223,1,223,107,677,677,224,1002,223,2,223,1005,224,674,101,1,223,223,4,223,99,226], 5);
+
+console.log('should be equal to 9386583');
